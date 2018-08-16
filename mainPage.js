4ae5afc02780b37a2b82e293414b0c9cda2644d4
David Wolak <awesomesauce712@gmail.com>
2018-08-16 13:32:25 -0700

    Add files via upload
    Changed the webpage look, made better buttons and added some animations

---
+++ mainPage.js
@@ -0,0 +1,11 @@
+
+
+function showLeft(){
+    document.getElementById("leftSplit").style.background = "red";
+   
+}
+
+function hideLeft()
+{
+    document.getElementById("leftSplit").style.background = "powderBlue";
+}
